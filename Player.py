c252975f1b017365f7fb419aaaacca4fdb0487ee
theswoderman <nicholas.swider@gmail.com>
2025-06-01 20:00:31 -0700

    small optimizations

--- Player.py
+++ Player.py
@@ -1,13 +1,17 @@
 import pygame
 from circleshape import *
 from constants import *
+from main import *
+
+
 
 class Player(CircleShape):
+
      def __init__(self,x,y):
           super().__init__(x,y,PLAYER_RADIUS)
-          self.rotation = 0
+          self.rotation = 0     
+
      
-     # in the player class
      def triangle(self): 
           forward = pygame.Vector2(0, 1).rotate(self.rotation)
           right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from Player import *
 
 
+
+
 def main():
      pygame.init()
      clock = pygame.time.Clock()
@@ -11,14 +13,18 @@ def main():
      print(f"Screen width: {SCREEN_WIDTH}")
      print(f"Screen height: {SCREEN_HEIGHT}")
      screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+     updatable = pygame.sprite.Group()
+     drawable = pygame.sprite.Group() 
+     Player.containers = (updatable, drawable)
      player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
      while True:
           for event in pygame.event.get():
                if event.type == pygame.QUIT:
                     return
           pygame.Surface.fill(screen, (0,0,0))
-          player.update(dt)
-          player.draw(screen)
+          updatable.update(dt)
+          for object in drawable:
+               object.draw(screen)
           pygame.display.flip()
           dt = clock.tick(60)/1000
 

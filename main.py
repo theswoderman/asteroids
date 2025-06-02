35d1faeb1c33ecd76b3ef480af8521dccd88c82f
theswoderman <nicholas.swider@gmail.com>
2025-06-01 19:05:11 -0700

    rotation

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
                if event.type == pygame.QUIT:
                     return
           pygame.Surface.fill(screen, (0,0,0))
+          player.update(dt)
           player.draw(screen)
           pygame.display.flip()
           dt = clock.tick(60)/1000

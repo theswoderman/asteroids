52b2510a7767b9a4037f23691b81ae8cf91b0e79
theswoderman <nicholas.swider@gmail.com>
2025-06-01 18:53:18 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from constants import *
+from Player import *
+
+
+def main():
+     pygame.init()
+     clock = pygame.time.Clock()
+     dt = 0
+     print("Starting Asteroids!")
+     print(f"Screen width: {SCREEN_WIDTH}")
+     print(f"Screen height: {SCREEN_HEIGHT}")
+     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+     while True:
+          for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                    return
+          pygame.Surface.fill(screen, (0,0,0))
+          player.draw(screen)
+          pygame.display.flip()
+          dt = clock.tick(60)/1000
+
+if __name__ == "__main__":
+     main()
